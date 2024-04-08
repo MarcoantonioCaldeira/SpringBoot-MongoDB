faa3f29e0827196b11b526f2dc7944e93c95f8ef
Marco Antonio Caldeira <caldeiran391@gmail.com>
2024-04-07 22:07:31 -0300

    finish my project

--- src/main/java/com/marcoantonio/workshopmongo/repository/PostRepository.java
+++ src/main/java/com/marcoantonio/workshopmongo/repository/PostRepository.java
@@ -12,5 +12,7 @@ public interface PostRepository extends MongoRepository<Post, String> {
     @Query("{ 'title' :  { $regex: ?0, $options: 'i' } }")
     List<Post> searchTitle(String text);
     List<Post> findByTitleContainingIgnoreCase(String text);
+
+    
 }
 
